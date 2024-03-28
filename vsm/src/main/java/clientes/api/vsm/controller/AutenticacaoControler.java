5999c004f19d142485bf4bb558662c0ab14bf099
Gabriel Godinho <gabrielpinheirosb77@hotmail.com>
2024-03-28 18:38:59 -0300

    Criando metodo para gerar Token

--- vsm/src/main/java/clientes/api/vsm/controller/AutenticacaoControler.java
+++ vsm/src/main/java/clientes/api/vsm/controller/AutenticacaoControler.java
@@ -1,6 +1,8 @@
 package clientes.api.vsm.controller;
 
 import clientes.api.vsm.domain.usuario.DadosAutenticacao;
+import clientes.api.vsm.domain.usuario.Usuario;
+import clientes.api.vsm.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,14 @@ public class AutenticacaoControler {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
        var authentication = manager.authenticate(token);
-       return ResponseEntity.ok().build();
+       return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 
